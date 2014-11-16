076ed47754c60cd289febacb09cd7149918e5e25
ilammy <a.lozovsky@gmail.com>
2014-11-16 12:52:04 +0200

    Added `dikt` wrapper class over dicts
    I hope to base configurations on it, should be handy.

--- utils.py
+++ utils.py
@@ -13,3 +13,30 @@ def make_enum(*names):
 
     enums = dict(zip(names, map(enum_type, names)))
     return type('Enum', (), enums)
+
+
+class dikt:
+    """A convenience wrapper that allows to access dict keys as attributes."""
+    def __init__(self, dict):
+        self.__dict__['dict'] = dict # avoiding infinite regression
+
+    def __str__(self): return str(self.dict)
+    def __repr__(self): return repr(self.dict)
+
+    def __getattr__(self, name):
+        if name not in self.dict:
+            raise AttributeError("No such attribute '%s'" % name)
+
+        return self.dict[name]
+
+    def __getitem__(self, name):
+        return self.__getattr__(name)
+
+    def __setattr__(self, name, value):
+        if name not in self.dict:
+            raise AttributeError("No such attribute '%s'" % name)
+
+        self.dict[name] = value
+
+    def __setitem__(self, name, value):
+        return self.__setattr__(name, value)
