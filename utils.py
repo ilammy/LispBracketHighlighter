20d9723e2eb0c10dcd6f8ee3f87f26df9daf8bd3
ilammy <a.lozovsky@gmail.com>
2014-11-16 12:51:11 +0200

    Improved hashing of enums
    Now they are actually distinct as they compare only to themselves.

--- utils.py
+++ utils.py
@@ -1,8 +1,15 @@
 def make_enum(*names):
+    """Constructs a new enumeration type for the given list of values."""
     class enum_type:
         def __init__(self, name): self.name = name
         def __str__(self): return self.name
         def __repr__(self): return self.name
+
+        def __eq__(self, other):
+            return (self is other)
+
+        def __hash__(self):
+            return hash((self.name, enum_type))
 
     enums = dict(zip(names, map(enum_type, names)))
     return type('Enum', (), enums)
