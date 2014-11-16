7ee732982e62ab18458c4515707f800be258a99f
ilammy <a.lozovsky@gmail.com>
2014-11-16 15:26:58 +0200

    Implemented unifying type matchers

--- unify.py
+++ unify.py
@@ -1,11 +1,30 @@
-def boolean():
-    pass
+#
+# Type matchers
+#
 
-def string():
-    pass
+def boolean(value):
+    """Matches booleans."""
+    if not isinstance(value, bool):
+        raise ValueError("'%r' is not a boolean" % value)
 
-def integer():
-    pass
+    return value
+
+
+def string(value):
+    """Matches strings (both ASCII and Unicode)."""
+    if not isinstance(value, str) and not isinstance(value, unicode):
+        raise ValueError("'%r' is not a string" % value)
+
+    return value
+
+
+def integer(value):
+    """Matches integers."""
+    if not isinstance(value, int) and not isinstance(value, long):
+        raise ValueError("'%r' is not an integer" % value)
+
+    return value
+
 
 def either():
     pass
